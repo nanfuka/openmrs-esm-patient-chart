730eab794d3c2d8e299f4e0b8edf915a04713c77
nanfuka <kalungi2k6@gmail.com>
2023-06-16 12:13:43 +0300

    Fix lint errors

--- packages/esm-patient-vitals-app/src/vitals/vitals.resource.tsx
+++ packages/esm-patient-vitals-app/src/vitals/vitals.resource.tsx
@@ -71,7 +71,7 @@ export function useVitals(patientUuid: string, includeBiometrics: boolean = fals
       }
       return url;
     },
-    [pageSize, concepts, patientUuid],
+    [concepts, patientUuid],
   );
 
   const { data, isValidating, setSize, error, size, mutate } = useSwrInfinite<
